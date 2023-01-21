03d1db2c6dd4e98fef1e72ea4b7feae410f6be2b
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-21 15:52:31 -0800

    updates

--- REST API/chatgpt.py
+++ REST API/chatgpt.py
@@ -1,15 +1,22 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-chatbot = Chatbot(config, conversation_id=None)
+chatbot = Chatbot(config)
 
 @app.route('/chat', methods=['POST'])
-
-# This is the function that will be called when the user sends a message to the chatbot
 def chat():
     request_data = request.get_json()
     message = request_data['message']
     response = chatbot.get_response(message)
+    return jsonify(response)
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    request_data = request.get_json()
+    message = request_data['message']
+    conversation_id = request_data.get('conversation_id')
+    parent_id = request_data.get('parent_id')
+    response = chatbot.ask(message, conversation_id, parent_id)
     return jsonify(response)
 
 if __name__ == '__main__':

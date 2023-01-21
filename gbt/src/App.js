5404455ee14d0b7ae3d498a849e689e86266dd82
Donald <donaldjewkes@gmail.com>
2023-01-21 13:25:15 -0800

    added simple chat-bot

--- gbt/src/App.js
+++ gbt/src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
+
+const steps = [
+  {
+    id: '0',
+    message: 'Welcome to react chatbot!',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'Bye!',
+    end: true,
+  },
+];
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ChatBot steps={steps} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

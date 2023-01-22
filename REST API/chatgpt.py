279a6256036e050608c1914ea0a45f759ee3d072
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-21 17:37:01 -0800

    redoing rest API

--- REST API/chatgpt.py
+++ REST API/chatgpt.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from revChatGPT.ChatGPT import Chatbot
 import json
 
@@ -6,19 +6,13 @@ app = Flask(__name__)
 config = json.load(open("config.json"))
 chatbot = Chatbot(config)
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    request_data = request.get_json()
-    message = request_data['message']
-    response = chatbot.get_response(message)
-    return jsonify(response)
-
 @app.route('/ask', methods=['POST'])
 def ask():
-    request_data = request.get_json()
-    message = request_data['message']
-    response = chatbot.ask(message, conversation_id, parent_id)
-    return jsonify(response)
+    user_input = request.json['user_input']
+    conversation_id = request.json.get('conversation_id')
+    parent_id = request.json.get('parent_id')
+    response = chatbot.ask(user_input, conversation_id, parent_id)
+    return response
 
 if __name__ == '__main__':
     app.run()

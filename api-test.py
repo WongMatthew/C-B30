a255cc0f8497218b68d71e0ec9e04babb929ac70
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-21 17:46:13 -0800

    REST API WORKS

--- api-test.py
+++ api-test.py
@@ -1,6 +1,7 @@
 import requests
 
-url = "http://127.0.0.1:5000"
+url = 'http://127.0.0.1:5000/ask'
+data = {'user_input': 'ask me these questions one by one and have a conversation with me about them: Modifying Rules And Assumptions What is the rule (or assumption) I live by that I would like to modify? How does this rule (or assumption) afect me in my day to day life?'}
 
-response = requests.post("http://127.0.0.1:5000/chat", json={'message': 'Hello'})
+response = requests.post(url, json=data)
 print(response.json())

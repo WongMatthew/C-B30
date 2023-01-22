367ed1add7eebd392fde71c1ccb6c6528d2de800
ymnglli <ymnglli@gmail.com>
2023-01-21 16:51:24 -0800

    changed prompt json file

--- rest-api/chatgpt.py
+++ rest-api/chatgpt.py
@@ -1,12 +1,11 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
     data = request.get_json()
     session_token = data.get("session_token")
-    conversation_id = data.get("conversation_id")
-    parent_id = data.get("parent_id")
 
     chatbot = Chatbot({ "session_token": session_token }, conversation_id, parent_id)
     prompt = data.get("prompt")

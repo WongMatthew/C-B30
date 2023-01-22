910a2fcc968737f669f1a0140ac73137ec012e2c
Donald <donaldjewkes@gmail.com>
2023-01-21 18:26:09 -0800

    tailwind

--- gbt/src/App.js
+++ gbt/src/App.js
@@ -5,21 +5,29 @@ import ChatAPI from './components/ChatAPI.js';
 import gratitudeSteps from './config/gratitude_steps';
 
 
+// function App() {
+//   return (
 
+//     <div className="App">
+//       <div className='text-blue font-xl'>
+//         testing
+//       </div>
+//       <header className="App-header">
+//         <ChatBot
+//           headerTitle="C-B3O"
+//           recognitionEnable={true}
+//           steps={gratitudeSteps}
+//           />
+//       </header>
+//     </div>
+//   );
+// }
 function App() {
   return (
-
-    <div className="App">
-      <div className='text-blue'>
-        testing
-      </div>
-      <header className="App-header">
-        <ChatBot
-          headerTitle="C-B3O"
-          recognitionEnable={true}
-          steps={gratitudeSteps}
-          />
-      </header>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Why isn't this working
+      </p>
     </div>
   );
 }

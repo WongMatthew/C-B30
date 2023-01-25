e6660781174784c2558d5c89c0d89e0c35e98cfd
Matt Wong <wongmatthew357@gmail.com>
2023-01-24 16:49:29 -0800

    isolate the message from chatGPT api response

--- REST API/chatgpt.py
+++ REST API/chatgpt.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from revChatGPT.ChatGPT import Chatbot
 from flask_cors import CORS
 import json
@@ -10,11 +10,20 @@ chatbot = Chatbot(config)
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    user_input = request.json['user_input']
-    conversation_id = request.json.get('conversation_id')
-    parent_id = request.json.get('parent_id')
+    user_input = request.json["user_input"]
+    conversation_id = request.json.get("conversation_id", None)
+    parent_id = request.json.get("parent_id", None)
     response = chatbot.ask(user_input, conversation_id, parent_id)
-    return json.dumps(response)
+    message = response.get('message')
+    return jsonify({"response": message})
+
+# @app.route('/ask', methods=['POST'])
+# def ask():
+#     user_input = request.json['user_input']
+#     conversation_id = request.json.get('conversation_id')
+#     parent_id = request.json.get('parent_id')
+#     response = chatbot.ask(user_input, conversation_id, parent_id)
+#     return json.dumps(response)
 
 if __name__ == '__main__':
     app.run()

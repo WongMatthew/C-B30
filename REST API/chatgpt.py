87f89f0aa26218a538ea69bcc99a7ebc4a273b7a
Matt Wong <49925170+WongMatthew@users.noreply.github.com>
2023-01-27 10:50:27 -0800

    remove testing statements, final version of rest api

--- REST API/chatgpt.py
+++ REST API/chatgpt.py
@@ -8,26 +8,20 @@ CORS(app)
 config = json.load(open("config.json"))
 chatbot = Chatbot(config)
 
-# @app.route('/ask', methods=['POST'])
-# def ask():
-#     request_data = request.get_json()
-#     print(request_data['user_input'])
-#     return jsonify({"response" : "hello"})
-
 @app.route('/ask', methods=['POST'])
 def ask():
     request_data = request.get_json()
     conversation_id = request_data['conversation_id']
     parent_id = request_data['parent_id']
     user_input = request_data['user_input']
-    print(user_input)
-    print(conversation_id)
-    print(parent_id)
-    response = chatbot.ask(user_input, None, None)
-    print(response)
-    print("hello6")
-    message = "balh blah"   
-    return jsonify({"response": "XXXXXX"})
+    response = chatbot.ask(user_input, conversation_id, parent_id)
+    message = response['message'] 
+    new_conversation_id = response['conversation_id']
+    new_parent_id = response['parent_id']
+    print("--------------------")
+    print("Check:" + message, new_conversation_id, new_parent_id)
+    print("--------------------")
+    return jsonify({"response": message, "conversation_id": new_conversation_id, "parent_id": new_parent_id})
 
 '''
 fix the conversation_id and parent_id
@@ -35,6 +29,13 @@ fix the conversation_id and parent_id
 supposed to be None
 
 https://prnt.sc/_0B7zerdbKyd
+
+^^ fixed!!
+
+Note: on the FE, need to pass in conversation_id and parent_id as None for the API call, afterwards, 
+save the conversation_id and parent_id from the response and pass it in for the next API call
+
+FOLLOW UP NOTE: DONT ACTUALLY NEED TO SAVE THE CONVERSATION_ID AND PARENT_ID, JUST PASS IN NONE FOR THE FIRST CALL
 '''
 
 # @app.route('/ask', methods=['POST'])

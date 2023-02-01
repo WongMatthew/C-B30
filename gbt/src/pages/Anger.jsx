e54254f429a75e26c4c5b3fbfaf9fa1c1715325a
AlanAyy <58347931+AlanAyy@users.noreply.github.com>
2023-02-01 17:09:08 -0500

    Update Anger.jsx

--- gbt/src/pages/Anger.jsx
+++ gbt/src/pages/Anger.jsx
@@ -4,7 +4,7 @@ import ChatBot from 'react-simple-chatbot';
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const bot_response_message = '';
+let bot_response_message = '';
 
 const Anger = () => {
   const [setConversationId] = useState(null);
@@ -37,39 +37,39 @@ const Anger = () => {
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <div className='flex flex-row w-full items-center justify-center'>
-    // BUG: If any step is missing a message or contains a trigger, the chatbot will not work
+    {/* BUG: If any step is missing a message or contains a trigger, the chatbot will not work */}
         <ChatBot
           steps={[
-            {
-              id: '1',
-              message: "Hello, welcome to the chatbot",
-              // trigger: 'user',
-            },
             // {
-            //   id: 'user',
-            //   user: true, //functions cannot be async or else it will create errors on chatbot, this has been left here as an example
-            //   trigger: async (output) => {
-            //     console.log(output.value);
-            //     if (output.value === 'shut up') {
-            //       //handleEnd();
-            //       return 'end';
-            //     }
-            //     bot_response_message = await handleUserInput(output.value);//main issue here
-            //     return 'repeat';
-            //   },
+            //   id: '1',
+            //   message: "Hello, welcome to the chatbot",
+            //   // trigger: 'user',
             // },
+            {
+              id: 'user',
+              user: true, //functions cannot be async or else it will create errors on chatbot, this has been left here as an example
+              trigger: async (output) => {
+                console.log(output.value);
+                if (output.value === 'shut up') {
+                  //handleEnd();
+                  return 'end';
+                }
+                const bot_response_message = await handleUserInput(output.value);//main issue here
+                return 'repeat';
+              },
+            },
             // {
             //   id: 'repeat',
             //   message: "bye",
             //   trigger: 'user',
             // },
-            {
-              id: 'end',
-              message: 'bye!',
-              //https://lucasbassetti.com.br/react-simple-chatbot/#/docs/steps    <-- link to documentation on the custom component, example is provided as well
-              // component: /*insert custom component element here, needs to assign bot_response_message using the async function (maybe display if possible) */,
-              end: true,
-            },
+            // {
+            //   id: 'end',
+            //   message: 'bye!',
+            //   //https://lucasbassetti.com.br/react-simple-chatbot/#/docs/steps    <-- link to documentation on the custom component, example is provided as well
+            //   // component: /*insert custom component element here, needs to assign bot_response_message using the async function (maybe display if possible) */,
+            //   end: true,
+            // },
           ]}
         />
       </div>
